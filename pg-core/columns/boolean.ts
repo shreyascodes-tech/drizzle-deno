fb48a441edecae09202f855d02d2b819ade57f63
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2023-04-07 21:48:25 +0530

    (pg-core) feat: add types for boolean column type

--- pg-core/columns/boolean.ts
+++ pg-core/columns/boolean.ts
@@ -1,6 +1,13 @@
 import { sql } from "../../sql.ts";
 import { customType } from "../columns.ts";
 import { PgBooleanBuilderInitial } from "https://esm.sh/drizzle-orm@0.23.8/pg-core/columns/boolean";
+export type {
+  PgBooleanBuilderInitial,
+  PgBoolean,
+  PgBooleanBuilder,
+  PgBooleanBuilderHKT,
+  PgBooleanHKT,
+} from "https://esm.sh/drizzle-orm@0.23.8/pg-core/columns/boolean";
 
 export function boolean<TName extends string>(
   name: TName
